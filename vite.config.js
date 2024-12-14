cd4989709945558e65f1c17c76a7bda14536db0a
Jrius Kaxibwe <30425075+Jrius4@users.noreply.github.com>
2024-12-14 14:20:12 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue';
 import { fileURLToPath, URL } from 'url';
-import ghPages from 'vite-plugin-gh-pages';
+import { ghPages } from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
